1d6721cee401246f2f0f09feab8a482c36982c83
jingxi <774874572@qq.com>
2021-06-01 16:52:35 +0800

    SCSS插入前

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
 
+const base = require('./webpack.config.base.js')
+
 module.exports = {
-    mode: 'development',
-    devtool: "inline-source-map",
-    devServer: {
-            contentBase: "./dist"
-          },
-    entry: './src/index.js',
-    output: {
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Jingxi',
-        template: 'src/assets/index.html'
-    })],
-    module: {
-            rules: [
-              {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-              }
-            ]
-          }
+  ...base,
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      ...base.module.rules,
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
